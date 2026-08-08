464f3b291c500225f1b63f4d41000b12adea4310
hkx8tp4xz5-blip <hkx8tp4xz5@privaterelay.appleid.com>
2026-08-08 14:38:00 +0800

    登录判断，用户账号密码是否正确

--- src/main/java/com/example/myruoyi/controller/LoginController.java
+++ src/main/java/com/example/myruoyi/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.example.myruoyi.controller;
 
 public class LoginController {
+
 }
--- src/main/java/com/example/myruoyi/service/SysLoginService.java
+++ src/main/java/com/example/myruoyi/service/SysLoginService.java
@@ -1,6 +1,7 @@
 package com.example.myruoyi.service;
 
 import com.example.myruoyi.common.TokenService;
+import com.example.myruoyi.domain.SysUser;
 import com.example.myruoyi.mapper.SysUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,21 @@ public class SysLoginService {
 
     @Autowired
     private SysUserMapper sysUserMapper;
+
     @Autowired
     private TokenService tokenService;
+
+    public String login(String username, String password) {
+        SysUser sysUser = sysUserMapper.selectByUsername(username);
+        if (username == null || password == null) {
+            throw new RuntimeException("用户不存在");
+        }
+        if (!sysUser.getPassword().equals(password)) {
+            throw new RuntimeException("密码错误");
+        }
+        return tokenService.createToken(sysUser);
+    }
+
 }
 
 
