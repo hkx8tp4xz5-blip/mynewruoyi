6200a67b857b79f06092d36981492ae339df5401
hkx8tp4xz5-blip <hkx8tp4xz5@privaterelay.appleid.com>
2026-08-18 14:58:55 +0800

    优化登录界面和逻辑

---
+++ src/main/resources/static/checkLogin.js
@@ -0,0 +1,27 @@
+var token = localStorage.getItem("token");
+if (token == null) {
+    location.href = "login.html"                // 没有token进入登录页
+} else {
+    fetch("/getInfo", {headers: {"Authorization": token}})
+        .then(function (response) {
+            if (response.status === 401) {
+                throw "登录失效"
+            }
+            return response.json();
+        })
+        .then(function (result) {
+            if (result.code !== 200) {
+                throw "登录失效"
+            }
+        })
+
+        .catch(function () {
+            localStorage.removeItem("token")  // 失败删除token
+            location.href = "login.html"           // 失败返回登录页
+        })
+}
+
+
+
+
+
